4f527e7e1cb2d2b9b639535b45401290782775fc
shwetank04 <56911869+shwetank04@users.noreply.github.com>
2022-01-15 19:15:16 +0530

    Update BalancedNumber.java

--- src/main/String/BalancedNumber.java
+++ src/main/String/BalancedNumber.java
@@ -17,32 +17,6 @@ and RHS = 0+0+6 = 6. LHS is equal to RHS.
 // { Driver Code Starts
 //Initial Template for Java
 
-import java.io.*;
-import java.util.*;
-class GfG
-{
-    public static void main (String[] args)
-    {
-        
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t-- > 0)
-        {
-            String N = sc.next ();
-
-            if (new Sol().balancedNumber (N))
-    		    System.out.println (1);
-    		else
-    		    System.out.println (0);
-        }
-        
-    }
-}// } Driver Code Ends
-
-
-//User function Template for Java
-
 class Sol
 {
     Boolean balancedNumber(String N)
