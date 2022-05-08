ae73b9e54579b837dec2c2598298077445866875
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-05-08 18:48:51 +0530

    Create TowerOfHanoi.java

---
+++ src/main/Recursion/TowerOfHanoi.java
@@ -0,0 +1,9 @@
+	public static void towerOfHanoi(int n, String src, String helper, String dest) {
+		if (n == 1) {
+			System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+			return;
+		}
+		towerOfHanoi(n - 1, src, dest, helper);
+		System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+		towerOfHanoi(n - 1, helper, src, dest);
+	}
