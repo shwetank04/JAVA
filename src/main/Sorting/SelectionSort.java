f6db79cf3adaafba6ae9d7d7257978fbc9c8e599
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-05-16 16:13:23 +0530

    Create SelectionSort.java

---
+++ src/main/Sorting/SelectionSort.java
@@ -0,0 +1,14 @@
+	// selection sort
+	public void selectionSort(int arr[]) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int smallest = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[smallest] > arr[j]) {
+					smallest = j;
+				}
+			}
+			int temp = arr[smallest];
+			arr[smallest] = arr[i];
+			arr[i] = temp;
+		}
+	}
