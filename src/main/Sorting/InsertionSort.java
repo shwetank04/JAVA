acecfb06964930214b28f15d93b370b496bf57ad
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-05-16 16:13:44 +0530

    Create InsertionSort.java

---
+++ src/main/Sorting/InsertionSort.java
@@ -0,0 +1,12 @@
+// Insertion sort
+	public void insertionSort(int arr[]) {
+		for (int i = 1; i < arr.length; i++) {
+			int current = arr[i];
+			int j = i - 1;
+			while (j >= 0 && current < arr[j]) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = current;
+		}
+	}
