9b6373b6f2c3d0314dc1eaff1682403db856900d
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-06-19 16:30:52 +0530

    Create CircularQueueArray.java

---
+++ src/main/Queue/CircularQueueArray.java
@@ -0,0 +1,61 @@
+package com.luv2code.hibernate.demo;
+
+public class CircularQueueArray {
+
+	static class Queue {
+		static int arr[];
+		static int size;
+		static int rear = -1;
+		static int front = -1;
+
+		Queue(int size) {
+			arr = new int[size];
+			this.size = size;
+		}
+
+		public static boolean isEmpty() {
+			return rear == -1 && front == -1;
+		}
+
+		public static boolean isFull() {
+			return (rear + 1) % size == front;
+		}
+
+		public static void add(int data) {
+			if (isFull()) {
+				System.out.println("full queue");
+				return;
+			}
+			// 1st element
+			if (front == -1) {
+				front = 0;
+			}
+			rear = (rear + 1) % size;
+			arr[rear] = data;
+		}
+
+		public static int remove() {
+			if (isEmpty()) {
+				System.out.println("empty queue");
+				return -1;
+			}
+			int result = arr[front];
+			// single element condition
+			if (rear == front) {
+				rear = front = -1;
+			} else {
+				front = (front + 1) % size;
+			}
+			return result;
+		}
+
+		public static int peek() {
+			if (isEmpty()) {
+				System.out.println("empty queue");
+				return -1;
+			}
+			return arr[front];
+		}
+	}
+
+}
