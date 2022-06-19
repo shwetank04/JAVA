2be273c6e99ea9acb2a2c58c758e3c28c48e997b
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-06-19 15:11:18 +0530

    Create QueueArray.java

---
+++ src/main/Queue/QueueArray.java
@@ -0,0 +1,50 @@
+package com.luv2code.hibernate.demo;
+
+public class QueueY {
+
+	static class Queue {
+		static int arr[];
+		static int size;
+		static int rear = -1;
+
+		Queue(int size) {
+			arr = new int[size];
+			this.size = size;
+		}
+
+		public static boolean isEmpty() {
+			return rear == -1;
+		}
+
+		public static void add(int data) {
+			if (rear == size - 1) {
+				System.out.println("full queue");
+				return;
+			}
+			rear++;
+			arr[rear] = data;
+		}
+
+		public static int remove() {
+			if (isEmpty()) {
+				System.out.println("empty queue");
+				return -1;
+			}
+			int front = arr[0];
+			for (int i = 0; i < rear; i++) {
+				arr[i] = arr[i + 1];
+			}
+			rear--;
+			return front;
+		}
+
+		public static int peek() {
+			if (isEmpty()) {
+				System.out.println("empty queue");
+				return -1;
+			}
+			return arr[0];
+		}
+	}
+
+}
