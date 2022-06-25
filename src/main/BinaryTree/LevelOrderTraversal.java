e9d8414374863b4f4e82a483bb1f8cd4433a8820
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-06-25 14:49:19 +0530

    Create LevelOrderTraversal.java

---
+++ src/main/BinaryTree/LevelOrderTraversal.java
@@ -0,0 +1,41 @@
+public class BinaryTrees {
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+  
+public static void levelOrder(Node root) {
+		if (root == null) {
+		}
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		q.add(null);
+		while (!q.isEmpty()) {
+			Node currNode = q.remove();
+			if (currNode == null) {
+				System.out.println();
+				if (q.isEmpty()) {
+					break;
+				} else {
+					q.add(null);
+				}
+			} else {
+				System.out.println(currNode.data + " ");
+				if (currNode.left != null) {
+					q.add(currNode.left);
+				}
+				if (currNode.right != null) {
+					q.add(currNode.right);
+				}
+			}
+
+		}
+	}
+}
