c9c64edf512bad8b93a5b3305ce4b7778ad5b44d
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-07-07 00:51:47 +0530

    Create PairofSum.java

---
+++ src/main/Arrays/PairofSum.java
@@ -0,0 +1,31 @@
+Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+
+
+Example 1:
+
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+Explanation: 
+arr[0] + arr[1] = 1 + 5 = 6 
+and arr[1] + arr[3] = 5 + 1 = 6.
+  
+  
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+        // code here
+        int pair =0;
+        int j;
+        for(int i=0;i<n;i++){
+            j= i+1;
+            while(j<=n-1){
+                if(arr[i] + arr[j] == k){
+                    pair++;
+                }
+                j++;
+            }
+        }
+        return pair;
+    }
+}
