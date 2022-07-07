e04382b7e9eed50b42c017f9ba6d7156d06cb08a
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-07-08 00:56:33 +0530

    Create LargestSubArray0.java

---
+++ src/main/Arrays/LargestSubArray0.java
@@ -0,0 +1,39 @@
+Given an array having both positive and negative integers. The task is to compute the length of the largest subarray with sum 0.
+
+Example 1:
+
+Input:
+N = 8
+A[] = {15,-2,2,-8,1,7,10,23}
+Output: 5
+Explanation: The largest subarray with
+sum 0 will be -2 2 -8 1 7.
+  
+  
+  
+
+class GFG {
+    // Returns length of the largest subarray
+    // with 0 sum
+    static int maxLen(int arr[], int n)
+    {
+        int max_len = 0;
+ 
+        // Pick a starting point
+        for (int i = 0; i < n; i++) {
+            // Initialize curr_sum for every
+            // starting point
+            int curr_sum = 0;
+ 
+            // try all subarrays starting with 'i'
+            for (int j = i; j < n; j++) {
+                curr_sum += arr[j];
+ 
+                // If curr_sum becomes 0, then update
+                // max_len
+                if (curr_sum == 0)
+                    max_len = Math.max(max_len, j - i + 1);
+            }
+        }
+        return max_len;
+    }
