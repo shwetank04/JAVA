5be80373598bce0bc59401fbb42d80232d40ead9
shwetank <56911869+shwetank04@users.noreply.github.com>
2022-07-26 01:32:47 +0530

    Update DuplicateElements.java

--- src/main/Arrays/DuplicateElements.java
+++ src/main/Arrays/DuplicateElements.java
@@ -20,27 +20,27 @@ in the given array.
 
 class Solution {
     public static ArrayList<Integer> duplicates(int arr[], int n) {
-        // code here
-        int j = 0;
-        Arrays.sort(arr);
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        for(int i=0;i<n;i++){
-            j = i+1;
-           while(j<n){
-            if(arr[i] == arr[j] && !result.contains(arr[i])){
-                result.add(arr[i]);
-                }
-                else{
-                     j += 1;
-                }
+        // First check all the values that are
+        // present in an array then go to that
+        // values as indexes and increment by
+        // the size of array
+        for (int i = 0; i < n; i++) {
+            int index = arr[i] % n;
+            arr[index] += n;
+        }
+
+        // Now check which value exists more
+        // than once by dividing with the size
+        // of array
+        int flag = 0;
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if ((arr[i] / n) > 1) {
+                ans.add(i);
+                flag = 1;
             }
         }
-        if(result.isEmpty()){
-            result.add(-1);
-            return result;
-        }
-        else{
-            return result;
-        }
+        if (flag == 0) ans.add(-1);
+        return ans;
     }
 }
