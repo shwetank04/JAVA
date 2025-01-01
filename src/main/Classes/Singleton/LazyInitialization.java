69580820b7a96eca737c4f25b03fe12530d3945b
shwetank <56911869+shwetank04@users.noreply.github.com>
2025-01-01 16:36:23 +0530

    Create LazyInitialization.java

---
+++ src/main/Classes/Singleton/LazyInitialization.java
@@ -0,0 +1,23 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(DbConnector.getInstance());
+	}
+}
+
+public class DbConnector {
+	
+	private static DbConnector instance;
+	DbConnector() {
+		
+	}
+	
+	public static DbConnector getInstance() {
+		if(instance == null) {
+			instance = new DbConnector();
+		}
+		return instance;
+	}
+	
+}
